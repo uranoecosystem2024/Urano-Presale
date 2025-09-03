e3cd0cdfc99438ece8e19a60a7e028815f096896
MedNewton <med.newton.2013@gmail.com>
2025-09-04 00:56:20 +0100

    .

--- src/components/SubscriptionModal.tsx
+++ src/components/SubscriptionModal.tsx
@@ -42,14 +42,14 @@ const SubscriptionModal: React.FC<SubscriptionModalProps> = ({ open, onClose }) 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
 
-        // Prevent submission if email is invalid
+        // Don't submit if email is invalid
         if (emailError) {
             return;
         }
 
-        // Perform Web3Forms submission programmatically after validation
+        // Perform the form submission to FormBold after validation
         const form = e.target as HTMLFormElement;
-        form.submit(); // This will trigger the form submission to Web3Forms API
+        form.submit(); // This triggers the form submission to FormBold API
     };
 
     return (
@@ -69,13 +69,13 @@ const SubscriptionModal: React.FC<SubscriptionModalProps> = ({ open, onClose }) 
                     display: 'flex',
                     flexDirection: 'column',
                     justifyContent: 'space-between',
-                    height: '65%', // Fixed height of the modal
-                    minHeight: '400px', // Set a minimum height for the modal
+                    height: '65%',
+                    minHeight: '400px',
                     maxHeight: '90%',
                 }}
             >
                 {/* Background Image */}
-                <Box sx={{ position: 'relative', width: '100%', height: "30%" }}>
+                <Box sx={{ position: 'relative', width: '100%', height: '30%' }}>
                     <Image
                         src={popupbg}
                         alt="Popup background"
@@ -91,37 +91,41 @@ const SubscriptionModal: React.FC<SubscriptionModalProps> = ({ open, onClose }) 
                         backdropFilter: 'blur(10px)',
                         p: 3,
                         borderTop: `1px solid ${alpha(theme.palette.common.white, 0.1)}`,
-                        height: "70%",
-
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "center",
+                        height: '70%',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
                     }}
                 >
-                    <Box sx={{
-                        width: "fit-content",
-                        display: "flex",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        borderRadius: 2,
-                        paddingX: 2,
-                        paddingY: 1,
-                        aspectRatio: 1,
-                        background: theme.palette.uranoGradient,
-                        marginTop: -6.5,
-                        border: `5px solid ${theme.palette.background.paper}`
-                    }}>
-                        <Typography variant="body1" sx={{
-                            fontWeight: 500,
-                            fontSize: "1.5rem",
-                            lineHeight: 1,
-                            color: "#2A6A69",
-                        }}>
+                    <Box
+                        sx={{
+                            width: 'fit-content',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            borderRadius: 2,
+                            paddingX: 2,
+                            paddingY: 1,
+                            aspectRatio: 1,
+                            background: theme.palette.uranoGradient,
+                            marginTop: -6.5,
+                            border: `5px solid ${theme.palette.background.paper}`,
+                        }}
+                    >
+                        <Typography
+                            variant="body1"
+                            sx={{
+                                fontWeight: 500,
+                                fontSize: '1.5rem',
+                                lineHeight: 1,
+                                color: '#2A6A69',
+                            }}
+                        >
                             1
                         </Typography>
                     </Box>
-                    <Stack width={"100%"} height={"100%"} alignItems={"center"} paddingY={2} gap={4}>
-                        <Stack width={"100%"} alignItems={"center"} gap={1}>
+                    <Stack width={'100%'} height={'100%'} alignItems={'center'} paddingY={2} gap={4}>
+                        <Stack width={'100%'} alignItems={'center'} gap={1}>
                             <Typography
                                 variant="h5"
                                 fontWeight={500}
@@ -130,95 +134,107 @@ const SubscriptionModal: React.FC<SubscriptionModalProps> = ({ open, onClose }) 
                                     background: theme.palette.uranoGradient,
                                     WebkitBackgroundClip: 'text',
                                     WebkitTextFillColor: 'transparent',
-                                    backgroundClip: 'text'
+                                    backgroundClip: 'text',
                                 }}
                             >
                                 Register with email
                             </Typography>
-                            <Typography
-                                variant="body1"
-                                color="text.secondary"
-                                textAlign="center"
-                            >
+                            <Typography variant="body1" color="text.secondary" textAlign="center">
                                 Create your account in seconds.
                                 <br />
                                 Just enter your email to get started.
                             </Typography>
                         </Stack>
-                        <form action="https://api.web3forms.com/submit" method="POST" style={{ width: "100%" }} onSubmit={handleSubmit}>
-                        <input type="hidden" name="access_key" value="5e98fa64-78e9-4400-815c-9c742cb8d35b" />
-                        <Stack width={"100%"} alignItems={"center"} gap={2}>
-                            <Stack width={"100%"} gap={1}>
-                                <FormControl fullWidth variant="outlined">
-                                    <InputLabel
-                                        htmlFor="component-outlined"
-                                        sx={{
-                                            // Default label color
-                                            color: emailError ? "#FF3665" : 'white',
-                                            '&.Mui-focused': {
-                                                color: emailError ? "#FF3665" : theme.palette.uranoGreen1.main, // Change this to your desired focused color
+                        <form
+                            onSubmit={handleSubmit} // Use handleSubmit function to validate email
+                            action="https://formbold.com/s/91LpA" // FormBold form URL
+                            method="POST"
+                            style={{ width: '100%' }}
+                        >
+                            {/* Hidden Access Key */}
+                            <input
+                                type="hidden"
+                                name="New submission"
+                                value="New submission for Urano Presale page" // Replace with your FormBold access key
+                            />
+                            {/* Email Field */}
+                            <Stack width={'100%'} alignItems={'center'} gap={2}>
+                                <Stack width={'100%'} gap={1}>
+                                    <FormControl fullWidth variant="outlined">
+                                        <InputLabel
+                                            htmlFor="component-outlined"
+                                            sx={{
+                                                // Default label color
+                                                color: emailError ? '#FF3665' : 'white',
+                                                '&.Mui-focused': {
+                                                    color: emailError ? '#FF3665' : theme.palette.uranoGreen1.main,
+                                                },
+                                            }}
+                                        >
+                                            E-mail
+                                        </InputLabel>
+                                        <OutlinedInput
+                                            id="component-outlined"
+                                            name="email" // Ensure name is "email" to match FormBold
+                                            value={email} // Bind the email value to the state
+                                            placeholder="Enter your e-mail"
+                                            label="E-mail"
+                                            type="email"
+                                            onChange={handleEmailChange}
+                                            sx={{
+                                                '& .MuiOutlinedInput-notchedOutline': {
+                                                    borderColor: emailError ? '#FF3665' : alpha(theme.palette.common.white, 0.2),
+                                                },
+                                                '&:hover .MuiOutlinedInput-notchedOutline': {
+                                                    borderColor: emailError ? '#FF3665' : alpha(theme.palette.common.white, 0.3),
+                                                },
+                                                '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                                    borderColor: emailError ? '#FF3665' : theme.palette.uranoGreen1.main,
+                                                },
+                                            }}
+                                        />
+                                    </FormControl>
+                                    {emailError && (
+                                        <Typography variant="body2" color="#FF3665">
+                                            {emailError}
+                                        </Typography>
+                                    )}
+                                </Stack>
+
+                                {/* Submit Button */}
+                                <Button
+                                    variant="contained"
+                                    type="submit"
+                                    sx={{
+                                        background: theme.palette.secondary.main,
+                                        borderRadius: '8px',
+                                        py: 1.5,
+                                        width: '100%',
+                                        fontWeight: 500,
+                                        color: theme.palette.text.primary,
+                                        '&:hover': {
+                                            background: theme.palette.uranoGreen1.main,
+                                            '& .registerButtonText': {
+                                                color: theme.palette.background.paper,
                                             },
+                                        },
+                                    }}
+                                >
+                                    <Typography
+                                        className="registerButtonText"
+                                        variant="body1"
+                                        sx={{
+                                            fontWeight: 400,
+                                            color: theme.palette.text.primary,
+                                            textTransform: 'none',
                                         }}
                                     >
-                                        E-mail
-                                    </InputLabel>
-                                    <OutlinedInput
-                                        id="component-outlined"
-                                        defaultValue={email}
-                                        placeholder="Enter your e-mail"
-                                        label="E-mail"
-                                        type="email"
-                                        onChange={handleEmailChange}
-                                        sx={{
-                                            '& .MuiOutlinedInput-notchedOutline': {
-                                                borderColor: emailError ? "#FF3665" : alpha(theme.palette.common.white, 0.2), // Set border color of the input
-                                            },
-                                            '&:hover .MuiOutlinedInput-notchedOutline': {
-                                                borderColor: emailError ? "#FF3665" : alpha(theme.palette.common.white, 0.3), // Border color on hover
-                                            },
-                                            '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
-                                                borderColor: emailError ? "#FF3665" : theme.palette.uranoGreen1.main, // Change border color when input is focused
-                                            },
-
-                                        }}
-                                    />
-                                </FormControl>
-                                {emailError && (
-                                    <Typography variant="body2" color="#FF3665">
-                                        {emailError}
+                                        Register
                                     </Typography>
-                                )}
+                                </Button>
                             </Stack>
-                            <Button
-                                variant="contained"
-                                type="submit"
-                                sx={{
-                                    background: theme.palette.secondary.main,
-                                    borderRadius: '8px',
-                                    py: 1.5,
-                                    width: "100%",
-                                    fontWeight: 500,
-                                    color: theme.palette.text.primary,
-                                    '&:hover': {
-                                        background: theme.palette.uranoGreen1.main,
-                                        "& .registerButtonText": {
-                                            color: theme.palette.background.paper,
-                                        }
-                                    },
-                                }}
-                            >
-                                <Typography className='registerButtonText' variant="body1" sx={{
-                                    fontWeight: 400,
-                                    color: theme.palette.text.primary,
-                                    textTransform: "none",
-                                }}>
-                                    Register
-                                </Typography>
-                            </Button>
-                        </Stack>
                         </form>
                     </Stack>
-
                 </Box>
             </Box>
         </Modal>
