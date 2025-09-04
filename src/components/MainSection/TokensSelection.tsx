604ead67630fa0713e3badc760f3809f2985c904
MedNewton <med.newton.2013@gmail.com>
2025-09-04 20:04:35 +0100

    .

--- src/components/MainSection/TokensSelection.tsx
+++ src/components/MainSection/TokensSelection.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Stack } from "@mui/material";
 import TokenSelectionTextField from "@/components/MainSection/TokenSelectionTextField";
 import usdc from "@/assets/images/usdc1.webp";
@@ -12,13 +12,18 @@ import urano from "@/assets/images/urano1.webp";
 const TokensSelection = () => {
     const [value, setValue] = useState(0);
     const [convertedValue, setConvertedValue] = useState(0);
+
+    useEffect(() => {
+        setConvertedValue(value * 33.3); // You can modify the multiplier as needed
+      }, [value]);
+      
     return (
         <Stack width={"100%"} direction={{xs: "column",lg:"row"}} justifyContent={"space-between"} alignItems={"center"} gap={{xs: 2, lg: 0}}>
             <Stack width={{xs:"100%",lg:"40%"}}>
                 <TokenSelectionTextField value={value} label="Pay with stablecoin" tokenIconSrc={usdc.src} tokenSymbol="USDC" onChange={(v) => setValue(v)} />
             </Stack>
             <Stack width={{xs:"100%",lg:"40%"}}>
-                <TokenSelectionTextField value={value * 3.33} label="Receive URANO" tokenIconSrc={urano.src} tokenSymbol="URANO" />
+                <TokenSelectionTextField value={convertedValue} label="Receive URANO" tokenIconSrc={urano.src} tokenSymbol="URANO" />
             </Stack>
 
         </Stack>
