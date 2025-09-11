550c7d33d964636736f7eb5d396769cdee25edff
MedNewton <med.newton.2013@gmail.com>
2025-09-11 19:09:51 +0100

    .

--- src/components/MainSection/Registration.tsx
+++ src/components/MainSection/Registration.tsx
@@ -11,95 +11,139 @@ const Registration = () => {
     const [open, setOpen] = useState(false);
     return (
         <>
-        <Stack width={"100%"} direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
-            <Stack width={{xs:"48%",lg:"40%"}} direction={"row"} alignItems={"center"} gap={{xs: 1, lg: 4}} sx={{
-                background: theme.palette.background.paper,
-                borderRadius: 2,
-                padding: 1,
-                border: `1px solid ${theme.palette.headerBorder.main}`,
-                cursor: "pointer",
-                "&:hover": {
-                    border: `1px solid ${theme.palette.uranoGreen1.main}`,
-                },
-            }}
-                onClick={() => setOpen(true)}
-            >
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: 1.5,
-                    paddingX: 2,
-                    paddingY: 1,
-                    aspectRatio: 1,
-                    background: theme.palette.uranoGradient,
-                }}>
-                    <Typography variant="body1" sx={{
-                        fontWeight: 500,
-                        fontSize: {xs: "1rem", lg: "1.5rem"},
-                        lineHeight: 1,
-                        color: "#2A6A69",
+            <Stack width={"100%"} direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
+                <Stack width={{ xs: "48%", lg: "35%" }} direction={"row"} alignItems={"center"} gap={{ xs: 1, lg: 2 }} sx={{
+                    background: theme.palette.background.paper,
+                    borderRadius: 2,
+                    padding: 1,
+                    border: `1px solid ${theme.palette.headerBorder.main}`,
+                    cursor: "pointer",
+                    "&:hover": {
+                        border: `1px solid ${theme.palette.uranoGreen1.main}`,
+                    },
+                }}
+                    onClick={() => setOpen(true)}
+                >
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 1.5,
+                        paddingX: 1,
+                        paddingY: 0.5,
+                        aspectRatio: 1,
+                        background: theme.palette.uranoGradient,
+                        backgroundColor: "yellow",
                     }}>
-                        1
-                    </Typography>
-                </Box>
-                <Typography variant="subtitle2" sx={{
-                    fontWeight: 500,
-                    fontSize: {xs: "0.75rem", lg: "0.875rem"},
-                    color: theme.palette.text.primary,
-                    width: "40%",
-                }}>
-                    Register With Email
-                </Typography>
-
-            </Stack>
-            <IoChevronForward size={24} color={theme.palette.text.primary} />
-            <Stack width={{xs:"48%",lg:"40%"}} direction={"row"} alignItems={"center"} gap={{xs: 1, lg: 4}} sx={{
-                background: theme.palette.background.paper,
-                borderRadius: 2,
-                padding: 1,
-                border: `1px solid ${theme.palette.headerBorder.main}`,
-            }}>
-                <Box sx={{
-                    display: "flex",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    borderRadius: 1.5,
-                    paddingX: 2,
-                    paddingY: 1,
-                    aspectRatio: 1,
-                    background: theme.palette.uranoGradient,
-                }}>
-                    <Typography variant="body1" sx={{
-                        fontWeight: 500,
-                        fontSize: {xs: "1rem", lg: "1.5rem"},
-                        lineHeight: 1,
-                        color: "#2A6A69",
-                    }}>
-                        2
-                    </Typography>
-                </Box>
-                <Stack width={"100%"}>
+                        <Typography variant="body1" sx={{
+                            fontWeight: 500,
+                            fontSize: { xs: "1rem", lg: "1.45rem" },
+                            lineHeight: 1,
+                            color: "#2A6A69",
+                        }}>
+                            1
+                        </Typography>
+                    </Box>
                     <Typography variant="subtitle2" sx={{
                         fontWeight: 500,
-                        fontSize: {xs: "0.75rem", lg: "0.875rem"},
+                        fontSize: { xs: "0.75rem", lg: "0.75rem" },
                         color: theme.palette.text.primary,
+                        width: "80%",
                     }}>
-                        Verify Identity
+                        Register with Email
                     </Typography>
-                    <Typography variant="subtitle2" sx={{
-                        fontWeight: 400,
-                        fontSize: {xs: "0.65rem", lg: "0.75rem"},
-                        color: theme.palette.text.secondary,
-                    }}>
-                        Coming Soon
-                    </Typography>
-                </Stack>
-            </Stack>
 
-        </Stack>
-        <SubscriptionModal open={open} onClose={() => setOpen(false)} />
-        
+                </Stack>
+                <IoChevronForward size={24} color={theme.palette.text.primary} style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }} />
+                <Stack width={{ xs: "48%", lg: "35%" }} direction={"row"} alignItems={"center"} gap={{ xs: 1, lg: 2 }} sx={{
+                    background: theme.palette.background.paper,
+                    borderRadius: 2,
+                    padding: 1,
+                    border: `1px solid ${theme.palette.headerBorder.main}`,
+                    cursor: "pointer",
+                    "&:hover": {
+                        border: `1px solid ${theme.palette.uranoGreen1.main}`,
+                    },
+                }}
+                    onClick={() => setOpen(true)}
+                >
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 1.5,
+                        paddingX: 1,
+                        paddingY: 0.5,
+                        aspectRatio: 1,
+                        background: theme.palette.uranoGradient,
+                        backgroundColor: "yellow",
+                    }}>
+                        <Typography variant="body1" sx={{
+                            fontWeight: 500,
+                            fontSize: { xs: "1rem", lg: "1.45rem" },
+                            lineHeight: 1,
+                            color: "#2A6A69",
+                        }}>
+                            2
+                        </Typography>
+                    </Box>
+                    <Typography variant="subtitle2" sx={{
+                        fontWeight: 500,
+                        fontSize: { xs: "0.75rem", lg: "0.75rem" },
+                        color: theme.palette.text.primary,
+                        width: "80%",
+                    }}>
+                        Verify your identity
+                    </Typography>
+
+                </Stack>
+                <IoChevronForward size={24} color={theme.palette.text.primary} style={{ marginLeft: "0.5rem", marginRight: "0.5rem" }} />
+                <Stack width={{ xs: "48%", lg: "35%" }} direction={"row"} alignItems={"center"} gap={{ xs: 1, lg: 2 }} sx={{
+                    background: theme.palette.background.paper,
+                    borderRadius: 2,
+                    padding: 1,
+                    border: `1px solid ${theme.palette.headerBorder.main}`,
+                    cursor: "pointer",
+                    "&:hover": {
+                        border: `1px solid ${theme.palette.uranoGreen1.main}`,
+                    },
+                }}
+                    onClick={() => setOpen(true)}
+                >
+                    <Box sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        borderRadius: 1.5,
+                        paddingX: 1,
+                        paddingY: 0.5,
+                        aspectRatio: 1,
+                        background: theme.palette.uranoGradient,
+                        backgroundColor: "yellow",
+                    }}>
+                        <Typography variant="body1" sx={{
+                            fontWeight: 500,
+                            fontSize: { xs: "1rem", lg: "1.45rem" },
+                            lineHeight: 1,
+                            color: "#2A6A69",
+                        }}>
+                            3
+                        </Typography>
+                    </Box>
+                    <Typography variant="subtitle2" sx={{
+                        fontWeight: 500,
+                        fontSize: { xs: "0.75rem", lg: "0.75rem" },
+                        color: theme.palette.text.primary,
+                        width: "80%",
+                    }}>
+                        Connect wallet & Buy
+                    </Typography>
+
+                </Stack>
+
+            </Stack>
+            <SubscriptionModal open={open} onClose={() => setOpen(false)} />
+
         </>
     )
 }
